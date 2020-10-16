fafdbb073c9d91c5a9919a897d7dcf887f236636
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2020-10-16 10:54:46 +0700

    Initial Commit

---
+++ src/if1/pkg10119033/latihan13/relational/IF110119033Latihan13Relational.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119033.latihan13.relational;
+
+/**
+ *
+ * @author
+ * NAMA     : Dandi Ahmadin
+ * KELAS    : IF-1
+ * NIM      : 10119033
+ * Deskripsi Program : Program ini berisi operator relational/perbandingan
+ */
+public class IF110119033Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("a >= b = " + (bil1 >= bil2));
+        System.out.println("a <= b = " + (bil1 <= bil2));
+    }
+    
+}
